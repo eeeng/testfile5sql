54273ae1ef63c397f8032ae2b906452cee0747f2
Ahmet Sari <106359636+eeeng@users.noreply.github.com>
2022-09-20 04:33:21 -0400

    Update docker.ps1

--- docker.ps1
+++ docker.ps1
@@ -1,3 +1,3 @@
 #p-sh docker container custom
-docker build --rm -t demo/flask-api:0.0 .
-docker run --rm --name demo-flask-api -p 5000:5000 demo/flask-api:0.0
+docker build --rm -t demo/data:0.0 .
+docker run --rm --name demo-data -p 5000:5000 demo/data:0.0
